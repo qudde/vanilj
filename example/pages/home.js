72824d359d708de7c6e99844ae3b61c36426fab4
Max Bondén <max.bonden@gmail.com>
2020-06-29 15:41:00 +0200

    Added buttons

--- example/pages/home.js
+++ example/pages/home.js
@@ -1,10 +1,12 @@
 import { FlexContainer } from "../../widgets/flexContainer";
 import { Container } from "../../widgets/container";
+import { Row } from "../../widgets/row";
 import { Text } from "../../widgets/text";
 import { withLayoutContainer } from "../widgets/withLayoutContainer";
+import router from "../state/routes";
 
 const titleText = () =>
-  new Text("The lightweight JavaScript framework.", null, {
+  new Text("The lightweight frontend framework.", null, {
     className: () => "heading-text",
     css: (context) => `
         .heading-text {
@@ -13,6 +15,7 @@ const titleText = () =>
             font-size: 43px;
             max-width: 90%;
             animation: heading-text-anim .6s ease;
+            align-self: flex-end;
         }
 
         @keyframes heading-text-anim {
@@ -20,6 +23,74 @@ const titleText = () =>
             to   { opacity: 1; transform: translateY(0px); }
           }      
     `
+  });
+
+const gettingStartedButton = () =>
+  new Container({
+    style: () => ({
+      width: "140px",
+      height: "44px",
+      backgroundColor: "white",
+      textAlign: "center",
+      lineHeight: "44px",
+      borderRadius: "40px",
+      paddingLeft: "20px",
+      paddingRight: "20px",
+      userSelect: "none",
+      fontSize: "17px",
+      boxShadow: "rgba(87, 127, 133, 0.4) 0px 4px 10px"
+    }),
+    className: () => "getting-started-text",
+    css: (context) => `
+        .getting-started-text {
+            animation:getting-started-text-anim 2s ease;
+        }
+
+        @keyframes getting-started-text-anim {
+            0% { opacity: 0; }
+            25% { opacity: 0; }
+            100% { opacity: 1; }
+          }      
+    `,
+    onPressed: () => router.navigate("/resources"),
+    builder: ({ children }) => children([new Text("Getting Started")])
+  });
+
+const gitHubButton = () =>
+  new Container({
+    style: () => ({
+      width: "90px",
+      height: "44px",
+      backgroundColor: "rgba(255,255,255,.2)",
+      textAlign: "center",
+      lineHeight: "44px",
+      borderRadius: "40px",
+      paddingLeft: "20px",
+      paddingRight: "20px",
+      userSelect: "none",
+      fontSize: "17px",
+      marginLeft: "10px",
+      boxShadow: "rgba(87, 127, 133, 0.4) 0px 4px 10px"
+    }),
+    className: () => "github-text",
+    css: (context) => `
+        .github-text {
+            animation:github-text-anim 3s ease;
+        }
+
+        @keyframes github-text-anim {
+            0% { opacity: 0; }
+            50% { opacity: 0; }
+            100% { opacity: 1; }
+          }      
+    `,
+    onPressed: () => router.navigate("/resources"),
+    builder: ({ children }) =>
+      children([
+        new Text("GitHub", {
+          color: "white"
+        })
+      ])
   });
 
 const HomePage = new Container({
@@ -34,11 +105,26 @@ const HomePage = new Container({
           style: (context) => ({
             width: "100%",
             height: "500px",
-            backgroundColor: "#000"
+            backgroundColor: "#000",
+            flexWrap: "wrap"
           }),
           alignItems: "center",
           justifyContent: "center",
-          builder: ({ children }) => children([titleText()])
+          builder: ({ children }) =>
+            children([
+              titleText(),
+              new Row({
+                style: {
+                  width: "100%",
+                  height: "120px",
+                  alignSelf: "flex-start",
+                  alignItems: "center",
+                  justifyContent: "center"
+                },
+                builder: ({ children }) =>
+                  children([gettingStartedButton(), gitHubButton()])
+              })
+            ])
         })
       ])
     ])
